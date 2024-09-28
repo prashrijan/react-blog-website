8d1f7c4b05b5d68b806ce9b79340b2ef820673be
Prashrijan <shresthaprashrijan@gmail.com>
2024-09-28 23:14:08 +1000

    env and appwrite setup

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+// good approach to import all env here and export it so that app doesnot crash if env is not ther
+
+const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    apwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
